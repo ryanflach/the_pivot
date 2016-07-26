548324def2c5e58f87fa4f9cb59436854b094d71
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-26 16:48:51 -0600

    Modify item link to undo link

--- spec/features/remove_item_from_cart_spec.rb
+++ spec/features/remove_item_from_cart_spec.rb
@@ -18,11 +18,11 @@ RSpec.feature "Remove an item from the cart" do
     # Then my current page should be "/cart"
     expect(current_path).to eq(cart_index_path)
     # And I should see a message styled in green
-    expect(page).to have_content("Successfully removed #{item.title} from your cart.")
+    expect(page).to have_content("Successfully removed #{item.title} from your cart. Undo.")
     # And the message should say "Successfully removed SOME_ITEM from your cart."
 
     # And the title "SOME_ITEM" should be a link to that item in case the user wants to add it back
-    expect(page).to have_link(item.title, href: item_path(item.id))
+    expect(page).to have_link("Undo", href: cart_items_path, method: :post)
     # And I should not see the item listed in cart
     within("tbody") do
       expect(page).not_to have_content("#{item.title}")
