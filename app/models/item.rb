98b9d927183a1b65bbe8b851214dd48f6ec896ba
Angela Lindow <angelalindow@gmail.com>
2016-07-26 09:11:08 -0600

    Close #2. Implement view all items.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :celebrity
-  validates :title, uniqueness: { scope: :celebrity_id }
+  validates :title, presence: true, uniqueness: { scope: :celebrity_id }
+  validates :price, presence: true
+
+  def celebrity_name
+    celebrity.name
+  end
+
 end
