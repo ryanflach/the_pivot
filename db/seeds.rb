7c939b161931d3ea1e9525f65f143ecb1d4bed64
Angela Lindow <angelalindow@gmail.com>
2016-07-26 12:41:07 -0600

    add seeds for item, category, and celebrity

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def category_selection
+  [{title: "Cleaning Supplies"}, {title: "Kitchen"},
+    {title: "Personal Hygiene"},{title: "Clothing"},
+    {title: "Tools"}, {title: "Refuse"},
+    {title: "Miscellaneous"}]
+end
+
+category_selection.each { |cat| Category.create(cat) }
+
+5.times do
+  Celebrity.create(name: Faker::Name.name)
+end
+
+20.times do
+  Item.create({
+      title: Faker::Commerce.product_name,
+      description: Faker::Hipster.paragraph,
+      price: Faker::Commerce.price.to_f,
+      image_path: Faker::Avatar.image,
+      category_id: Category.all.sample.id,
+      celebrity_id: Celebrity.all.sample.id
+    })
+end
