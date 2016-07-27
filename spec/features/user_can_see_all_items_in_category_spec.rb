f627a45c011a9aca2328c24046a80f3ac1898826
Angela Lindow <angelalindow@gmail.com>
2016-07-26 18:30:41 -0600

    Close #27. Implement partial for items list.

--- spec/features/user_can_see_all_items_in_category_spec.rb
+++ spec/features/user_can_see_all_items_in_category_spec.rb
@@ -5,12 +5,12 @@ RSpec.feature "User can see all items in a category" do
     celebrity = Celebrity.create(name: "Tom Cruise")
 
     cat1 = Category.create(title: "Kitchen")
-    cat1.items.create(title: "Knife", description: "Steel with wood handle", price: 100, celebrity: celebrity)
-    cat1.items.create(title: "Bowl", description: "Steel", price: 50, celebrity: celebrity)
+    cat1.items.create(title: "Knife", description: "Steel with wood handle", price: 100, image_path: "www", celebrity_id: celebrity.id)
+    cat1.items.create(title: "Bowl", description: "Steel", price: 50, image_path: "www", celebrity: celebrity)
 
     cat2 = Category.create(title: "Outdoor")
-    cat2.items.create(title: "Shovel", description: "Garden Shovel", price: 20.00, celebrity: celebrity)
-    cat2.items.create(title: "Saw", description: "Circular", price: 400, celebrity: celebrity)
+    cat2.items.create(title: "Shovel", description: "Garden Shovel", price: 20.00, image_path: "www", celebrity: celebrity)
+    cat2.items.create(title: "Saw", description: "Circular", price: 400, image_path: "www", celebrity: celebrity)
 
     visit category_path(cat2.title)
 
