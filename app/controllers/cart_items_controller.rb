35b403428cc9a2f139d5e67b4693f1fd9dfcbbd0
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-27 13:13:23 -0600

    Add ability to update quantity of items in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -6,4 +6,9 @@ class CartItemsController < ApplicationController
     flash[:success] = "#{item.title} added to cart successfully. Cart now contains #{@cart.total_items} items."
     redirect_to cart_index_path
   end
+
+  def update
+    @cart.contents[params[:id].to_s] = params[:item][:quantity].to_i
+    redirect_to cart_index_path
+  end
 end
