05025fbf9dd47eb8c9ea7b45c55c18695b8b44cf
Angela Lindow <angelalindow@gmail.com>
2016-07-28 09:29:35 -0600

    Add login/out styling align forms add 'logged in as <username>' to navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      flash[:notice] = "Login information incorrect."
+      flash[:danger] = "Login information incorrect."
     end
   end
 
