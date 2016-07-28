8b7c8c9ea0b1845b240dbaa9c77732bfadb529c2
Angela Lindow <angelalindow@gmail.com>
2016-07-27 20:53:56 -0600

    close #25 implement user can view past orders

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :cart_items, only: [:create, :update]
   resources :cart, only: [:index]
   resources :items, only: [:index, :show]
+  resources :orders, only: [:index]
   get '/:title', to: 'categories#show', as: :category
 end
