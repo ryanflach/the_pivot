abe8fc9a7f677b37418466b9786ba060b4362c44
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-28 17:02:35 -0600

    Close #70. Add ability to checkout from cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,24 @@
 class OrdersController < ApplicationController
+  before_action :verify_logged_in, only: [:index]
+
   def index
-    if current_user
-      @orders = current_user.orders
-    else
-      flash[:danger] = "Please login to view your orders"
-      redirect_to login_path
+    @orders = current_user.orders
+  end
+
+  def create
+    save_order
+    session[:cart] = nil
+    flash[:success] = "Order was successfully placed"
+    redirect_to orders_path
+  end
+
+  private
+
+  def save_order
+    order = Order.create(user: current_user)
+    @cart.all_items.each do |item|
+      order.items << Item.find(item.id)
     end
   end
+
 end
