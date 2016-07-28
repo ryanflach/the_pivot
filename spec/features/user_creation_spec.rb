b8b90a01ea1d5ca16a8d289df87451a8b52a7574
Lane Winham <mlwinham12@gmail.com>
2016-07-27 21:07:36 -0600

    Create account.
    Add user new/show views, add user controller actions, add current_user

---
+++ spec/features/user_creation_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "User can be created" do
+  scenario "user can visit create account form" do
+
+    visit new_user_path
+
+    fill_in "Username", with: "lane"
+    fill_in "Email", with: "lane@nil.com"
+    fill_in "Password", with: "password"
+    click_button "Create Account"
+
+    expect(page).to have_content "Logged in as lane"
+    expect(page).to have_link "Logout"
+    expect(page).to have_link "Cart"
+    
+    expect(page).not_to have_content "Login"
+  end
+end
