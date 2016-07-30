31b5a9165794bda43627685a336d86a68217fc61
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-30 12:23:28 -0600

    Fix test

--- spec/features/user/user_cannot_visit_admin_dashboard_spec.rb
+++ spec/features/user/user_cannot_visit_admin_dashboard_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature "Registered user cannot view the admin dashboard" do
 
     visit admin_dashboard_index_path
 
-    expect(current_path).to eq(login_path)
+    expect(current_path).to eq(dashboard_path)
     expect(page).to have_content "You cannot access admin content!"
 
     expect(page).to have_link "Logout"
