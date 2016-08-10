4c55c88c563f1434a8fa773c1b3a73f3d8caeb58
Ryan Flach <ryanflach@users.noreply.github.com>
2016-08-09 21:02:27 -0700

    Fix login form issue (#128)
    Fix issue with shared login form to show e-mail path if error in creating user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
   def new_user_path?
-    request.path == new_user_path
+    request.path == new_user_path || formatted_referrer == new_user_path
   end
 
   def admin_edit?
     current_admin? && request.path == edit_user_path(current_user)
+  end
+
+  private
+
+  def formatted_referrer
+    '/' + request.referrer.split('/')[-2..-1].join('/') if request.referrer
   end
 end
