452a16f81c45303ae4cf482dae86dd61c897ee4d
Garrett <garrett.smestad@gmail.com>
2016-08-21 01:03:20 -0600

    Add JS to improve navbar & page responsiveness.
    - Add JavaScript to events.js to make site fully responsive when navbar hamburger is opened/closed.
    - Add JavaScript to events.js to make fite fully responsive when window is resized.
    - Delete repetitive/unnecessary/unused CSS in stylesheets.

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -14,5 +14,18 @@ $(document).ready(function(){
     });
   });
 
+  $(document.body).css('padding-top', $('#topnavbar').height() + 10);
+
+  $(window).resize(function(){
+    $(document.body).css('padding-top', $('#topnavbar').height() + 10);
+  });
+
+  $(document).on('shown.bs.collapse', function () {
+    $(document.body).css('padding-top', $('#topnavbar').height() + 10);
+  });
+
+  $(document).on('hidden.bs.collapse', function () {
+    $(document.body).css('padding-top', $('#topnavbar').height() - 10);
+  });
 
 });
