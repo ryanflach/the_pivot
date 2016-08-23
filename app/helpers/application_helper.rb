b3f5872fe729070766661c1ee8215e6d09457180
matthewrpacker <matthewrpacker@gmail.com>
2016-08-23 13:54:17 -0600

    Add test scenarios for admin - Platform admin can login - Platform admin can view their dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,10 @@ module ApplicationHelper
     current_user && current_user.venue_admin?
   end
 
+  def venue_admin_only?
+    current_user && current_user.venue_admin? && !current_user.platform_admin?
+  end
+
   def registered_customer?
     current_user && current_user.registered_customer?
   end
