e99dc9f1ed4e44a593c40620aa2f7ff06b5612c9
matthewrpacker <matthewrpacker@gmail.com>
2016-08-23 21:58:16 -0600

    Add cancel buttons - Add cancel buttons for visitors - Add cancel buttons for registered customers - Add cancel buttons for venue admins

---
+++ spec/features/admin/admin_can_use_cancel_buttons_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+RSpec.feature "Admin can use cancel buttons" do
+  scenario "admin clicks cancel from manage account page" do
+    venue = create(:venue)
+    admin = venue.admin
+
+    allow_any_instance_of(ApplicationController).
+      to receive(:current_user).
+      and_return(admin)
+
+    visit admin_dashboard_index_path
+
+    click_on "Manage My Account"
+
+    expect(page).to have_content "Edit Account"
+    expect(current_path).to eq edit_user_path(admin)
+
+    click_on "Cancel"
+
+    expect(page).to have_content "Admin Dashboard"
+    expect(current_path).to eq admin_dashboard_index_path
+  end
+
+  scenario "admin clicks cancel from manage venue page" do
+    venue = create(:venue)
+    admin = venue.admin
+
+    allow_any_instance_of(ApplicationController).
+      to receive(:current_user).
+      and_return(admin)
+
+    visit admin_dashboard_index_path
+
+    click_on "Manage Venue"
+
+    expect(page).to have_content "Edit Venue"
+    expect(current_path).to eq edit_admin_venue_path(venue.name.downcase)
+
+    click_on "Cancel"
+
+    expect(current_path).to eq admin_dashboard_index_path
+  end
+
+  scenario "admin clicks cancel from new event page" do
+    venue = create(:venue)
+    admin = venue.admin
+
+    allow_any_instance_of(ApplicationController).
+      to receive(:current_user).
+      and_return(admin)
+
+    visit venue_path(venue.name.downcase)
+
+    click_on "Add New Event"
+
+    expect(page).to have_content "Add New Event"
+    expect(current_path).to eq new_admin_event_path
+
+    click_on "Cancel"
+
+    expect(current_path).to eq venue_path(venue.name.downcase)
+  end
+
+  scenario "admin clicks cancel from new event page" do
+    venue = create(:venue)
+    admin = venue.admin
+
+    allow_any_instance_of(ApplicationController).
+      to receive(:current_user).
+      and_return(admin)
+
+    visit admin_dashboard_index_path
+
+    click_on "View My Events"
+
+    expect(page).to have_content venue.name
+    expect(current_path).to eq venue_path(venue.name.downcase)
+
+    click_on "Back"
+
+    expect(current_path).to eq admin_dashboard_index_path
+
+    visit venues_path
+
+    within "#venue-#{venue.id}" do
+      click_on "View Venue"
+    end
+
+    expect(current_path).to eq venue_path(venue.name.downcase)
+
+    click_on "Back"
+
+    expect(current_path).to eq venues_path
+  end
+end
